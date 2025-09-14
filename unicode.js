6e7ac690de9c33ab929767943b2ed99d18739227
Dani <rufus6410@gmail.com>
2025-09-14 18:20:23 -0500

    Completed practice problems.

--- unicode.js
+++ unicode.js
@@ -86,15 +86,17 @@ Assign the result to a variable named swappedString.
 //Starter Code
 // Task 1
 let inputString1 = "Code";
-let firstCodePoint; // Your code here
-let thirdCodePoint; // Your code here
+let firstCodePoint = inputString1.charCodeAt(1); // Your code here
+let thirdCodePoint = inputString1.charCodeAt(3); // Your code here
 
 // Task 2
-let wordFromCodePoints; // Your code here
+let wordFromCodePoints = String.fromCharCode(72) + String.fromCharCode(101) + String.fromCharCode(108) + String.fromCharCode(108); // Your code here
 
 // Task 3
 let inputString2 = "Launch";
-let swappedString; // Your code here
+let codePoint1 = inputString2.charCodeAt(0);
+let codePoint2 = inputString2.charCodeAt(5);
+let swappedString = String.fromCharCode(codePoint2) + inputString2.slice(1,5) + String.fromCharCode(codePoint1); // Your code here
 
 // Log all results
 console.log({
